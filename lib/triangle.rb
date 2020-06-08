b653c63c193b543cc76ade3ccf176ebb05277b22
dlee3427 <david.lee.sl2634@yale.edu>
2020-06-08 00:02:16 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
   # write code here
-end
+
+  attr_reader :x, :y, :z 
+
+  def initialize(x, y, z)
+    @x = x 
+    @y = y 
+    @z = z 
+  end 
+
+  def kind 
+    triangle_def
+    if x == y && y == z 
+      :equilateral 
+    elsif x == y || x == z || y == z
+      :isosceles
+    else 
+      :scalene 
+    end 
+  end 
+
+  def triangle_def
+    triangle = [(z + y > x), (x + y > z), (x + z > y)]
+    [x, y, z].each do |s| 
+      triangle == false if s <= 0 
+    raise TriangleError if triangle.include?(false)
+    end
+  end
+
+  class TriangleError < StandardError    
+  end
+
+end 
